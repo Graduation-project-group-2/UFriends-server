98885afb4394e10fdbd71c8fe9c132fe640e99d4
danpung2 <danpung_2@naver.com>
2022-11-25 18:25:11 +0900

    로그인 수정

--- src/main/java/com/gachon/ufriendsserver/api/common/security/TokenProvider.java
+++ src/main/java/com/gachon/ufriendsserver/api/common/security/TokenProvider.java
@@ -1,28 +1,27 @@
 package com.gachon.ufriendsserver.api.common.security;
 
-import com.gachon.ufriendsserver.api.domain.Member;
+import com.gachon.ufriendsserver.api.domain.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
+@Slf4j
 @Service
 public class TokenProvider {
-    private static final String SECRET_KEY = "UFriends_SECRET_KEY";
+    private static final String SECRET_KEY = "NMA8JPctFuna59f5";
 
-    public String create(Member member){
+    public String create(User user){
         Date expiryDate = Date.from(
                 Instant.now()
                         .plus(1, ChronoUnit.DAYS)
         );
-        return Jwts.builder().signWith(SignatureAlgorithm.HS512, SECRET_KEY)
-                .setSubject(String.valueOf(member.getMemberId()))
-                .setIssuer("U-Friends").setIssuedAt(new Date())
-                .setExpiration(expiryDate).compact();
+        return Jwts.builder().signWith(SignatureAlgorithm.HS512, SECRET_KEY).setSubject(user.getEmail()).setIssuer("Ufriends").setIssuedAt(new Date()).setExpiration(expiryDate).compact();
     }
 
     public String validateAndGetUserId(String token){
